218970da28e53d32d65d52804267ce86b563507e
Matheus Henrique da Silva Ramos <matheus10213454@gmail.com>
2024-12-15 02:34:42 -0300

    Update Cod_pull-dow.ino

--- Exercício_em_Casa_1/Cod_pull-dow.ino
+++ Exercício_em_Casa_1/Cod_pull-dow.ino
@@ -1,1 +1,18 @@
-Montagem Básica com LEDs e Botão
+// Definindo os pinos
+const int ledPin = 13; // Pino do LED
+const int buttonPin = 5; // Pino do Botão
+
+void setup() {
+  // Configurando o pino do Led como saída, e o do Botão, como entrada
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  digitalWrite(ledPin, LOW); // Estado inicial do LED é em baixo (LOW)
+}
+
+void loop() {
+  if (digitalRead(buttonPin) == HIGH) { // Se o botão for precionado*********************
+    digitalWrite(ledPin, HIGH); // Acende o LED
+  } else { // Se o botão não for precionado
+    digitalWrite(ledPin, LOW); // Apaga o LED
+  }
+}
