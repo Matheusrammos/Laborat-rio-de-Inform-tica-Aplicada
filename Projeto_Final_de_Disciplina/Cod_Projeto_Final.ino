04e9e18516cdd0993726892b4efac6fa385227a4
Matheus Henrique da Silva Ramos <matheus10213454@gmail.com>
2025-02-01 03:25:32 -0300

    Update Cod_Projeto_Final.ino

--- Projeto_Final_de_Disciplina/Cod_Projeto_Final.ino
+++ Projeto_Final_de_Disciplina/Cod_Projeto_Final.ino
@@ -1,3 +1,5 @@
+#include <TimerOne.h>  // Biblioteca para usar Timer
+
 // Definindo pinos e algumas variáveis
 const int ledPins[] = { 3, 4, 5, 6, 7, 8, 9, 10 };  // Pinos onde os LEDs estão conectados (Pinos dos 6 LEDs)
 const int buttonPin = 13;                           // Pino do botão
@@ -6,6 +8,15 @@ bool a = false;                                     // Variável iterruptora
 long x;                                             // Número de piscadas
 int p;                                              // Tempo de cada piscada
 int c = 0;                                          // LED excolhido
+int som[] = { 58, 108 };
+
+bool z = false;  // Modo Tigrgrinho
+int counter = 0;
+bool estadoLEDs = false;
+int vitorias = 0;
+int chance;
+int sorte;
+
 
 
 void setup() {
@@ -13,7 +24,7 @@ void setup() {
   pinMode(buttonPin, INPUT);
 
   // Configura o pino do buzzer como saída
-  pinMode(buzzerPin, OUTPUT); 
+  pinMode(buzzerPin, OUTPUT);
 
   // Configura os pinos dos LEDs como saída
   for (int i = 0; i < 8; i++) {
@@ -28,55 +39,77 @@ void setup() {
 }
 
 
-void loop() {
-  for (int counter = 0; counter <= 6;) {
-    Serial.println(counter);         // Mostrar esse número no Serial Monitor
-    digitalWrite(ledPins[c], HIGH);  // Acende o respectivo LED
 
-    if (digitalRead(buttonPin) == HIGH) {  // Se o botão for pressionado
-      counter++;
-      delay(80);
-    }
-
-    if ((digitalRead(buttonPin) == LOW) && (counter >= 1 && counter <= 4)) {
-      digitalWrite(ledPins[c], LOW);   // Apaga o mesmo
-      c++;                             // O "interruptor" marcará 'true'
-      tone(buzzerPin, 58);             // Toca a nota correspondente
-      delay(60);                       // Espera 60 mili segundos 
-      noTone(buzzerPin);               // Deixa de tocar a nota
-
-      if (c == 6) {
-        c = 0;
+void piscarLEDs() {
+  if ((z == true) && (a == false)) {
+    estadoLEDs = !estadoLEDs;  // Alterna o estado dos LEDs
+    for (int i = 0; (i < 6); i++) {
+      if (i == c) {
+        i++;
       }
-      counter = 0;
-    } else if ((counter > 4) && ((digitalRead(buttonPin) == LOW) or (digitalRead(buttonPin) == HIGH))) {
-      a = !a;
+      digitalWrite(ledPins[i], estadoLEDs);
     }
   }
+}
+
+
+
+void giraRoleta() {
 
   // Se o "interrruptor" marcar "true"
   if (a == true) {
-    x = random(41, 591);       // Gerar um número aleatório de 41 a 590
-    Serial.println(x);         // Mostrar esse número no Serial Monitor
+    if (z == true) {
+      Serial.println((x % 6) - 1);  // Mostrar esse número no Serial Monitor
+      if (vitorias < 2) {
+        chance = 80;
+      } else if (vitorias < 5) {
+        chance = 50;
+        vitorias = vitorias--;
+      } else {
+        chance = 30;
+        vitorias = vitorias - 2;
+      }
+
+      sorte = random(0, 101);
+      Serial.println(sorte);  // Mostrar esse número no Serial Monitor
+      if (sorte <= chance) {
+        while (x <= 10){
+        x = ((c % 6) + 6 * sorte + 1);
+        }
+        vitorias++;
+      } else {
+        do {
+          x = random(41, 591);
+        } while (((x % 6) - 1) == c);
+      }
+      Serial.println(vitorias);  // Mostrar esse número no Serial Monitor
+
+
+    } else {
+      x = random(41, 591);  // Gerar um número aleatório de 41 a 590
+      Serial.println(x);    // Mostrar esse número no Serial Monitor
+    }
+
+
 
     // Acende e apaga os LEDs sequencialmente
     for (int i = 0; i < 7; i++) {
       // i = 6 será considerado como estado temporário. Quando isso ocorrer, a roleta, após o LED do pino 8 desligar, resetará para o LED do pino 3
-      if (i == 6) {            // Estado temporário
-        i = 0;                 // Resetado para o pino 3
+      if (i == 6) {  // Estado temporário
+        i = 0;       // Resetado para o pino 3
       }
 
       p = 1000 / x;                    // O tempo de piscada ficará maior a cada ciclo, pois o valor de "x" diminuirá a cada repetição
       digitalWrite(ledPins[i], HIGH);  // Acende o respectivo LED
 
       if (digitalRead(ledPins[i]) == HIGH) {
-        tone(buzzerPin, 58);   // Toca a nota correspondente
-        delay(p/2);            // Espera metade do tempo de piscada         
-        noTone(buzzerPin);     // Deixa de tocar a nota
-      }   
-      delay(p/2);                      // Espera metade do tempo de piscada                 
-      digitalWrite(ledPins[i], LOW);   // Apaga o mesmo
-      x--;                             // O valor de "x" diminui em 1
+        tone(buzzerPin, som[z]);  // Toca a nota correspondente
+        delay(p / 2);             // Espera metade do tempo de piscada
+        noTone(buzzerPin);        // Deixa de tocar a nota
+      }
+      delay(p / 2);                   // Espera metade do tempo de piscada
+      digitalWrite(ledPins[i], LOW);  // Apaga o mesmo
+      x--;                            // O valor de "x" diminui em 1
 
       // Indicando o LED escolhido
       if (x == 0) {
@@ -87,18 +120,18 @@ void loop() {
           delay(100);
           digitalWrite(ledPins[i], HIGH);
 
-          tone(buzzerPin, 120);            // Toca a nota correspondente
-          delay(60);                       // Espera 60 mili segundos 
-          noTone(buzzerPin);               // Deixa de tocar a nota
+          tone(buzzerPin, 120);  // Toca a nota correspondente
+          delay(60);             // Espera 60 mili segundos
+          noTone(buzzerPin);     // Deixa de tocar a nota
 
           delay(40);
           digitalWrite(ledPins[i], LOW);
           delay(100);
           digitalWrite(ledPins[i], HIGH);
 
-          tone(buzzerPin, 350);            // Toca a nota correspondente
-          delay(60);                       // Espera 60 mili segundos 
-          noTone(buzzerPin);               // Deixa de tocar a nota
+          tone(buzzerPin, 350);  // Toca a nota correspondente
+          delay(60);             // Espera 60 mili segundos
+          noTone(buzzerPin);     // Deixa de tocar a nota
 
         } else {
           digitalWrite(ledPins[6], HIGH);
@@ -107,29 +140,200 @@ void loop() {
           delay(100);
           digitalWrite(ledPins[i], HIGH);
 
-          tone(buzzerPin, 500);            // Toca a nota correspondente
-          delay(60);                       // Espera 60 mili segundos 
-          noTone(buzzerPin);               // Deixa de tocar a nota
+          tone(buzzerPin, 500);  // Toca a nota correspondente
+          delay(60);             // Espera 60 mili segundos
+          noTone(buzzerPin);     // Deixa de tocar a nota
 
           delay(40);
           digitalWrite(ledPins[i], LOW);
           delay(100);
           digitalWrite(ledPins[i], HIGH);
 
-          tone(buzzerPin, 300);            // Toca a nota correspondente
-          delay(60);                       // Espera 60 mili segundos 
-          noTone(buzzerPin);               // Deixa de tocar a nota
+          tone(buzzerPin, 300);  // Toca a nota correspondente
+          delay(60);             // Espera 60 mili segundos
+          noTone(buzzerPin);     // Deixa de tocar a nota
         }
 
+
+        if (digitalRead(buttonPin) == HIGH) {
+          // Ativa modo Tigrinho
+          z = !z;
+          vitorias = 0;
+          digitalWrite(ledPins[6], HIGH);
+          digitalWrite(ledPins[7], HIGH);
+
+          c = 0;      // Reseta o índice de LEDs
+          if (z == true){
+            digitalWrite(ledPins[i], LOW);
+
+            introModoTigrin();
+            a = false;  // Desativa o modo de roleta
+            loop();
+          }
+
+        } 
         delay(4940);
         digitalWrite(ledPins[i], LOW);
+        
 
         // Finaliza a roleta
         digitalWrite(ledPins[6], LOW);
         digitalWrite(ledPins[7], LOW);
-        a = false;                     // Desativa o modo de roleta
-        c = 0;                         // Reseta o índice de LEDs
-        i = 7;                         // Força a saída do comando for
+        a = false;  // Desativa o modo de roleta
+        c = 0;      // Reseta o índice de LEDs
+        i = 7;      // Força a saída do comando for
+      }
+    }
+  }
+}
+
+
+
+void introModoTigrin() {
+  // Acende e apaga os LEDs sequencialmente
+  for (int i = 0; (i < 7) && (z == true); i++) {
+    p = 130;  // O tempo de piscada ficará maior a cada ciclo, pois o valor de "x" diminuirá a cada repetição
+
+    // i = 6 será considerado como estado temporário. Quando isso ocorrer, a roleta, após o LED do pino 8 desligar, resetará para o LED do pino 3
+    if (i == 6) {  // Estado temporário
+      i = 0;       // Resetado para o pino 3
+      c++;
+    }
+
+    if ((c == 2) && (i == 1)) {
+      digitalWrite(ledPins[5], LOW);  // Apaga o mesmo
+    } else if (c == 3){
+      if (i == 1){
+        digitalWrite(ledPins[4], LOW);  // Apaga o mesmo
+      } else if (i == 2) {
+        digitalWrite(ledPins[5], LOW);  // Apaga o mesmo
+      }
+
+    } else if (c == 4){
+      if (i == 1){
+        digitalWrite(ledPins[3], LOW);  // Apaga o mesmo
+      } else if (i == 2) {
+        digitalWrite(ledPins[4], LOW);  // Apaga o mesmo
+      } else if (i == 3) {
+        digitalWrite(ledPins[5], LOW);  // Apaga o mesmo
+      }
+
+    } else if (c == 5){
+      if (i == 1){
+        digitalWrite(ledPins[2], LOW);  // Apaga o mesmo
+      } else if (i == 2) {
+        digitalWrite(ledPins[3], LOW);  // Apaga o mesmo
+      } else if (i == 3) {
+        digitalWrite(ledPins[4], LOW);  // Apaga o mesmo
+      } else if (i == 4){
+        digitalWrite(ledPins[5], LOW);  // Apaga o mesmo
+      } else if (i == 5){
+        digitalWrite(ledPins[0], LOW);  // Apaga o mesmo
+      }
+
+    } else if (c == 6){
+      c = 0;
+
+      vitorias++;
+      if (vitorias == 2){
+        counter = 0;  
+        vitorias = 0;
+        digitalWrite(ledPins[6], LOW);
+        digitalWrite(ledPins[7], LOW);
+        delay(500);
+        return;
+      }
+    }
+
+    digitalWrite(ledPins[i], HIGH);  // Acende o respectivo LED
+    if (digitalRead(ledPins[i]) == HIGH) {
+      tone(buzzerPin, som[z]);  // Toca a nota correspondente
+      delay(p / 2);             // Espera metade do tempo de piscada
+      noTone(buzzerPin);        // Deixa de tocar a nota
+    }
+    delay((p / 2)+(p / 2) * c);                 // Espera metade do tempo de piscada
+    digitalWrite(ledPins[i - c], LOW);  // Apaga o mesmo
+  }
+}
+
+
+
+void loop() {
+  for (int counter = 0; counter <= 6;) {
+    digitalWrite(ledPins[6], LOW);
+    digitalWrite(ledPins[7], LOW);
+    if (z == true) {
+      Timer1.initialize(1200000);          // Configura Timer para 400ms (400.000 microssegundos)
+      Timer1.attachInterrupt(piscarLEDs);  // Ativa interrupção a cada 500ms
+      Serial.println(counter);             // Mostrar esse número no Serial Monitor
+
+      if (estadoLEDs == true) {
+        digitalWrite(ledPins[c], LOW);  // Acende o respectivo LED
+      } else {
+        digitalWrite(ledPins[c], HIGH);  // Acende o respectivo LED
+      }
+      if (estadoLEDs == true) {
+        digitalWrite(ledPins[c - 1], HIGH);  // Acende o respectivo LED
+      } else {
+        digitalWrite(ledPins[c - 1], LOW);  // Acende o respectivo LED
+      }
+
+      if (digitalRead(buttonPin) == HIGH) {  // Se o botão for pressionado
+        counter++;
+        delay(80);
+      }
+
+      if ((digitalRead(buttonPin) == LOW) && (counter >= 1 && counter <= 4)) {
+        if (estadoLEDs == true) {
+          digitalWrite(ledPins[c], HIGH);  // Acende o respectivo LED
+        } else {
+          digitalWrite(ledPins[c], LOW);  // Acende o respectivo LED
+        }
+
+        c++;
+        if (c == 6) {
+          c = 0;
+        }
+
+        tone(buzzerPin, 108);
+        delay(60);          // Espera 60 mili segundos
+        noTone(buzzerPin);  // Deixa de tocar a nota
+        counter = 0;
+
+      } else if ((counter > 4) && ((digitalRead(buttonPin) == LOW) || (digitalRead(buttonPin) == HIGH))) {
+        a = !a;
+        counter = 0;
+        Timer1.stop();
+        giraRoleta();
+      }
+
+
+    } else {
+      Serial.println(counter);         // Mostrar esse número no Serial Monitor
+      digitalWrite(ledPins[c], HIGH);  // Acende o respectivo LED
+
+      if (digitalRead(buttonPin) == HIGH) {  // Se o botão for pressionado
+        counter++;
+        delay(80);
+      }
+
+      if ((digitalRead(buttonPin) == LOW) && (counter >= 1 && counter <= 4)) {
+        digitalWrite(ledPins[c], LOW);  // Apaga o mesmo
+        c++;                            // O "interruptor" marcará 'true'
+        tone(buzzerPin, som[z]);            // Toca a nota correspondente
+        delay(60);                      // Espera 60 mili segundos
+        noTone(buzzerPin);              // Deixa de tocar a nota
+
+        if (c == 6) {
+          c = 0;
+        }
+        counter = 0;
+
+      } else if ((counter > 4) && ((digitalRead(buttonPin) == LOW) or (digitalRead(buttonPin) == HIGH))) {
+        a = !a;
+        counter = 0;
+        Timer1.stop();
+        giraRoleta();
       }
     }
   }
